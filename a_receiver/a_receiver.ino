dc6382fae56e63d9b4c2a1bc8bc6c0d914050732
LeoTheHuman <leothehuman@gmail.com>
2015-04-19 15:40:10 +0100

    Added a display, more data and acks

--- a_receiver/a_receiver.ino
+++ a_receiver/a_receiver.ino
@@ -1,22 +1,34 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-const int n = 2;
+const int n = 6;
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
 
 struct Data
 {
   int a[n];  
 } data;
 
+int packets = 0;
 void printData(const struct Data& d)
 {
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.println(packets);
   for (int i = 0; i < n - 1; ++i)
   {
-    Serial.print(d.a[i]);
-    Serial.print(" ");
+    display.println(d.a[i]);
+//    Serial.print(d.a[i]);
+//    Serial.print(" ");
   }
-  Serial.println(d.a[n - 1]);
+  display.println(d.a[n - 1]);
+//  Serial.println(d.a[n - 1]);
+  display.display();
 }
 
 RF24 radio(9,10);
@@ -27,20 +39,35 @@ const uint64_t pipe = 0xF0F0F0F0E2LL;
 void setup(void)
 {
   radio.begin();
-  radio.setAutoAck(false);
+  radio.setAutoAck(true);
   radio.setDataRate(RF24_250KBPS);
   
   radio.openReadingPipe(1, pipe);
   radio.startListening(); 
   Serial.begin(115200);
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
 }
 
+int thispackets = 0;
+unsigned long lastsec = 0;
 void loop(void)
 {
   while (radio.available())
   {
+    unsigned long newsec = millis() / 1000;
+    if (newsec != lastsec)
+    {
+      lastsec = newsec;
+      packets = thispackets;
+      thispackets = 0;
+    }
+
     radio.read(&data, sizeof(Data));
     printData(data);
+    ++thispackets;
   }
 }
 // vim:cin:ai:sts=2 sw=2 ft=cpp
--- a_transmitter/a_transmitter.ino
+++ a_transmitter/a_transmitter.ino
@@ -2,11 +2,11 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-const int n = 2;
+const int n = 6;
 
 struct Data
 {
-  int a[n];  
+  int a[n];
 } data;
 
 void printData(const struct Data& d)
@@ -27,7 +27,7 @@ const uint64_t pipe = 0xF0F0F0F0E2LL;
 void setup(void)
 {
   radio.begin();
-  radio.setAutoAck(false);
+  radio.setAutoAck(true);
   radio.setDataRate(RF24_250KBPS);
   
   radio.openWritingPipe(pipe);
@@ -39,10 +39,10 @@ void loop(void)
 {
   for (int i = 0; i < n; ++i)
   {
-    data.a[i] = map(analogRead(i), 0, 1023, 1000, 2000);
+    data.a[i] = map(analogRead(i), 0, 895, 1000, 2000);
   }
   printData(data);
   radio.write(&data, sizeof(Data));
-  delay(10);
+//  delay(10);
 }
 // vim:cin:ai:sts=2 sw=2 ft=cpp
